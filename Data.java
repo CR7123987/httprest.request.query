7d2cd373dd50590e3950fa664b4523c7bdf809b0
CR7123987 <127672459+CR7123987@users.noreply.github.com>
2023-03-13 12:43:47 +0530

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class Data implements Serializable{
+	public String city;
+	public String name;
+	public int estimated_cost;
+	public UserRating user_rating;
+	public int id;
+}
